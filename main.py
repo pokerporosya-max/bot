cdf5ab59e08ebc6eddb34edaac2d5cc7b4f2f533
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 17:50:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from db import init_db
 
+from handlers import start, profile, bonus, games, dice, cactus, transfer, admin, top, help
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+async def main():
+    await init_db()
+
+    dp.include_router(start.router)
+    dp.include_router(profile.router)
+    dp.include_router(bonus.router)
+    dp.include_router(games.router)
+    dp.include_router(dice.router)
+    dp.include_router(cactus.router)
+    dp.include_router(transfer.router)
+    dp.include_router(admin.router)
+    dp.include_router(top.router)
+    dp.include_router(help.router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
