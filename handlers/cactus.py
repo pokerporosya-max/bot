e10bdca09c92443250bf273e5616748d4620a8c8
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 17:52:46 +0300

    Update cactus.py

--- handlers/cactus.py
+++ handlers/cactus.py
@@ -1,1 +1,13 @@
+import random
+from aiogram import Router, F
+from aiogram.types import Message
+from db import update_cactus
 
+router = Router()
+
+@router.message(F.text == "🌵 Полить")
+async def cactus(message: Message):
+    grow = random.randint(1, 12)
+    await update_cactus(message.from_user.id, grow)
+
+    await message.answer(f"🌵 Кактус вырос +{grow} см")
