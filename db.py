5d2f0a01620d882cf7496bd4bfd17f6d77c2d9c8
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 17:50:11 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,48 @@
+import asyncpg
+from config import DATABASE_URL
 
+pool = None
+
+async def init_db():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+    async with pool.acquire() as conn:
+        await conn.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id BIGINT PRIMARY KEY,
+            username TEXT,
+            balance INT DEFAULT 1000,
+            cactus INT DEFAULT 0,
+            games INT DEFAULT 0,
+            wins INT DEFAULT 0,
+            loses INT DEFAULT 0
+        )
+        """)
+
+async def add_user(user_id, username):
+    async with pool.acquire() as conn:
+        await conn.execute("""
+        INSERT INTO users (user_id, username)
+        VALUES ($1, $2)
+        ON CONFLICT (user_id) DO NOTHING
+        """, user_id, username)
+
+async def get_user(user_id):
+    async with pool.acquire() as conn:
+        return await conn.fetchrow("SELECT * FROM users WHERE user_id=$1", user_id)
+
+async def update_balance(user_id, amount):
+    async with pool.acquire() as conn:
+        await conn.execute("UPDATE users SET balance = balance + $1 WHERE user_id=$2", amount, user_id)
+
+async def update_game(user_id, win=False):
+    async with pool.acquire() as conn:
+        if win:
+            await conn.execute("UPDATE users SET wins = wins + 1, games = games + 1 WHERE user_id=$1", user_id)
+        else:
+            await conn.execute("UPDATE users SET loses = loses + 1, games = games + 1 WHERE user_id=$1", user_id)
+
+async def update_cactus(user_id, grow):
+    async with pool.acquire() as conn:
+        await conn.execute("UPDATE users SET cactus = cactus + $1 WHERE user_id=$2", grow, user_id)
