efb9494dcccb0616fd8f733033bb43d8951fc2bc
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 21:51:23 +0300

    Update db.py

--- db.py
+++ db.py
@@ -15,7 +15,8 @@ def init_db():
         balance BIGINT DEFAULT 0,
         games INT DEFAULT 0,
         wins INT DEFAULT 0,
-        loses INT DEFAULT 0
+        loses INT DEFAULT 0,
+        bonus_time BIGINT DEFAULT 0
     )
     """)
 
@@ -25,15 +26,18 @@ def get_user(user_id, name=None):
 
     if not user:
         cur.execute(
-            "INSERT INTO users (user_id, name, balance) VALUES (%s, %s, %s)",
-            (user_id, name, 0)
+            "INSERT INTO users (user_id, name) VALUES (%s, %s)",
+            (user_id, name)
         )
         return get_user(user_id, name)
 
     return user
 
 def update_balance(user_id, amount):
-    cur.execute("UPDATE users SET balance = balance + %s WHERE user_id=%s", (amount, user_id))
+    cur.execute(
+        "UPDATE users SET balance = balance + %s WHERE user_id=%s",
+        (amount, user_id)
+    )
 
 def update_game(user_id, win=False):
     cur.execute("UPDATE users SET games = games + 1 WHERE user_id=%s", (user_id,))
@@ -41,3 +45,9 @@ def update_game(user_id, win=False):
         cur.execute("UPDATE users SET wins = wins + 1 WHERE user_id=%s", (user_id,))
     else:
         cur.execute("UPDATE users SET loses = loses + 1 WHERE user_id=%s", (user_id,))
+
+def set_bonus_time(user_id, t):
+    cur.execute("UPDATE users SET bonus_time=%s WHERE user_id=%s", (t, user_id))
+
+def reset_db():
+    cur.execute("TRUNCATE TABLE users RESTART IDENTITY")
