c22b36f2604759a3b4b02354cc0ce02e55dcaea6
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 17:53:03 +0300

    Update bonus.py

--- handlers/bonus.py
+++ handlers/bonus.py
@@ -1,1 +1,10 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from db import update_balance
 
+router = Router()
+
+@router.message(F.text == "🎁 Бонус")
+async def bonus(message: Message):
+    await update_balance(message.from_user.id, 2500)
+    await message.answer("🎁 Вам начислено +2500 🍬")
