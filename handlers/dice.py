0bad839c6a3be262cff94dfe6d1957ae20356784
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 18:31:52 +0300

    Update dice.py

--- handlers/dice.py
+++ handlers/dice.py
@@ -1,27 +1,38 @@
 import random
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message
 from db import update_balance, update_game
 
 router = Router()
 
-@router.message(lambda m: m.text.startswith("кубик"))
-async def dice(message: Message):
-    try:
-        bet = int(message.text.split()[1])
-    except:
+@router.message(F.text.startswith("кубик"))
+async def dice_game(message: Message):
+
+    parts = message.text.split()
+
+    if len(parts) < 2:
         return await message.answer("Используй: кубик <ставка>")
 
-    roll = random.randint(1, 6)
+    try:
+        bet = int(parts[1])
+    except:
+        return await message.answer("Ставка должна быть числом")
 
+    # отправляем "бросок"
+    dice_msg = await message.answer_dice(emoji="🎲")
+    roll = dice_msg.dice.value
+
+    # победа
     if roll in [4, 6]:
         win = bet * 2
-        await update_balance(message.from_user.id, win)
-        await update_game(message.from_user.id, True)
 
-        await message.answer(f"🎲 {roll}\n🎉 Победа +{win} 🍬")
+        await update_balance(message.from_user.id, win)
+        await update_game(message.from_user.id, win=True)
+
+        await message.answer(f"🎲 Выпало: {roll}\n🎉 Победа +{win} 🍬")
+
     else:
         await update_balance(message.from_user.id, -bet)
-        await update_game(message.from_user.id, False)
+        await update_game(message.from_user.id, win=False)
 
-        await message.answer(f"🎲 {roll}\n💀 Проигрыш -{bet} 🍬")
+        await message.answer(f"🎲 Выпало: {roll}\n💀 Проигрыш -{bet} 🍬")
