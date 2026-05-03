1efe8ab7063d6aadae5477ab506f3594e28a8837
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 17:51:51 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,9 @@
+from aiogram import Router
+from aiogram.types import Message
+from db import add_user
 
+router = Router()
+
+@router.message()
+async def register_user(message: Message):
+    await add_user(message.from_user.id, message.from_user.username)
