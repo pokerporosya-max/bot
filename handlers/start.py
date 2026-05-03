69d34cb8cfdc4eee77b74dec64f8682911899b24
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 20:13:43 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,34 +1,32 @@
 from aiogram import Router, F
-from aiogram.types import Message
-from db import get_user
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from db import add_user
 
 router = Router()
 
-# 👤 ПРОФИЛЬ (из группы и ЛС через кнопку)
-@router.message(F.text == "👤 Профиль")
-async def profile(message: Message):
+@router.message(F.text.lower().in_({"/start", "start"}))
+async def start(message: Message):
 
-    user = await get_user(message.from_user.id)
-
-    if not user:
-        return await message.answer("❌ Профиль не найден")
-
-    cactus_value = user["cactus"]
-
-    if cactus_value >= 100:
-        cactus_text = f"{cactus_value / 100:.2f} м"
-    else:
-        cactus_text = f"{cactus_value} см"
-
-    text = (
-        f"👤 Профиль\n\n"
-        f"Имя: @{user['username']}\n"
-        f"ID: {user['user_id']}\n\n"
-        f"💰 Баланс: {user['balance']} 🍬\n"
-        f"🌵 Кактус: {cactus_text}\n\n"
-        f"🎮 Сыграно: {user['games']}\n"
-        f"🏆 Победы: {user['wins']}\n"
-        f"💀 Поражения: {user['loses']}"
+    # добавляем пользователя в БД
+    await add_user(
+        message.from_user.id,
+        message.from_user.username
     )
 
-    await message.answer(text)
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="➕ Добавить бота в группу",
+                url="https://t.me/GachyxBot?startgroup=true"
+            )
+        ]
+    ])
+
+    await message.answer(
+        "👋 Привет!\n\n"
+        "Добро пожаловать в игровой бот 🍬\n"
+        "Зарабатывай конфеты, играй и поднимайся в топ 🏆\n\n"
+        "Добавь бота в группу, чтобы играть с друзьями 👇",
+        reply_markup=kb
+    )
