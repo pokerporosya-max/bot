33f3967b08f8bc84e2c7086dcdc8ba7f8e5984ca
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 17:52:11 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,1 +1,24 @@
+from aiogram import Router
+from aiogram.types import Message
+from db import get_user
 
+router = Router()
+
+@router.message(lambda m: m.text == "👤 Профиль")
+async def profile(message: Message):
+    user = await get_user(message.from_user.id)
+
+    await message.answer(
+f"""👤 Профиль
+
+Имя: @{user['username']}
+ID: {user['user_id']}
+
+💰 Баланс: {user['balance']} 🍬
+🌵 Кактус: {user['cactus']}
+
+🎮 Сыграно: {user['games']}
+🏆 Победы: {user['wins']}
+💀 Поражения: {user['loses']}
+"""
+    )
