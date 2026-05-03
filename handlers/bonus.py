0695bf6651cea72da8adbdc030261cacd8b5b83f
pokerporosya-max <pokerporosya@gmail.com>
2026-05-03 18:28:37 +0300

    Update bonus.py

--- handlers/bonus.py
+++ handlers/bonus.py
@@ -1,10 +1,29 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from db import update_balance
 
 router = Router()
 
 @router.message(F.text == "🎁 Бонус")
 async def bonus(message: Message):
+
+    # если не ЛС — отправляем переход
+    if message.chat.type != "private":
+        kb = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(
+                text="🚀 Перейти в бота",
+                url="https://t.me/YOUR_BOT_USERNAME"
+            )]
+        ])
+
+        return await message.answer(
+            "🎁 Бонус\n\n"
+            "Чтобы получить награду, перейди в личные сообщения с ботом.\n\n"
+            "⚠️ Бонус можно получить только внутри бота",
+            reply_markup=kb
+        )
+
+    # если ЛС — выдаём бонус
     await update_balance(message.from_user.id, 2500)
-    await message.answer("🎁 Вам начислено +2500 🍬")
+
+    await message.answer("🎁 Ты получил: 2500 🍬")
